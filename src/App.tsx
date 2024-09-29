d535ae330a0060fdcf91cf404a2c9afa8938a12b
sergio.pena <sergio.pena.orozco@gmail.com>
2024-09-29 00:29:58 -0600

    Add order total component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import MenuItem from "./components/MenuItem"
 import OrderContents from "./components/OrderContents"
+import OrderTotals from "./components/OrderTotals"
+import TipPercentageForm from "./components/TipPercentageForm"
 import { menuItems } from "./data/db"
 import useOrder from "./hooks/useOrder"
 
 function App() {
 
-  const { order, addItem, removeItem } = useOrder()
+  const { order, addItem, removeItem, tip, setTip, placeOrder } = useOrder()
     
   return (
     <>
@@ -26,10 +28,26 @@ function App() {
           </div>
         </div>
         <div className="border border-dashed border-slate-300 p-5 rounded-lg space-y-10">
-          <OrderContents 
-            order={order}
-            removeItem={removeItem}
-          />
+          {order.length ? (
+            <>
+              <OrderContents 
+                order={order}
+                removeItem={removeItem}
+              />
+              <TipPercentageForm
+                setTip={setTip}
+                tip ={tip}
+              />
+              <OrderTotals
+                order={order}
+                tip={tip}
+                placeOrder={placeOrder}
+              />
+            </>
+          ): (
+            <p className="text-center">La orden esta vacia</p>
+          )}
+          
         </div>
       </main>
     </>
