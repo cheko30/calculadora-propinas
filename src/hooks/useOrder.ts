279149474c6fee25e6378ff3bd534a0f7d617dec
sergio.pena <sergio.pena.orozco@gmail.com>
2024-09-28 23:25:53 -0600

    Add order detail component

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -5,11 +5,28 @@ export default function useOrder() {
     const [order, setOrder] = useState<OrderItem[]>([])
 
     const addItem = (item: MenuItem) => {
-        console.log(item)
+        const itemExist = order.find(orderItem => orderItem.id === item.id)
+        if(itemExist) {
+            const updateOrder = order.map(orderItem => orderItem.id === item.id 
+                    ? {...orderItem, quantity: orderItem.quantity + 1} 
+                    : orderItem
+                )
+            setOrder(updateOrder)
+        } else {
+            const newItem = {...item, quantity: 1}
+            setOrder([...order, newItem])
+        }
+        
+    }
+
+    const removeItem = (id: MenuItem['id']) => {
+        setOrder(order.filter(item => item.id !== id))
     }
 
     return {
-        addItem
+        order,
+        addItem,
+        removeItem,
     }
 
 }
